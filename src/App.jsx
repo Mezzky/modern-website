4e19291a38d56ee58a70c481d01e9402bae94958
Mezzky <ryansahadha@gmail.com>
2024-05-11 13:38:51 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">Something</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
