e96ad95772fd567abf24377d120a10229fcd04d2
Mezzky <ryansahadha@gmail.com>
2024-05-22 14:01:35 +0800

    Add: Button

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
       <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Button className="mt-10">Something</Button>
+        <Button className="mt-10">Someone</Button>
       </div>
       <ButtonGradient />
     </>
